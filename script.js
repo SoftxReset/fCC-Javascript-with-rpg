75ce47fc78b5c77a21b70e6adde1c209e775f47b
Khlea <123595388+SoftxReset@users.noreply.github.com>
2024-03-07 22:13:09 -0800

    Update script.js
    "moved notes to README to make it easier to check for mistakes as the code wasn't executing as expected compared to in the fCC project window. Also testing if the code will properly update from VS to Github." - test failed ...

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-/* JavaScript interacts with the HTML using the Document Object Model, 
-or DOM. The DOM is a tree of objects that represents the HTML. 
-You can access the HTML using the document object, which represents your entire HTML document.
-The querySelector() method takes a CSS selector as an argument and 
-returns the first element that matches that selector.
-*/
-
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -46,24 +39,6 @@ const monsters = [
     health: 300
   }
 ]
-
-/*
-In your role playing game, you will be able to visit different 
-locations like the store, the cave, and the town square.
-You will need to create a data structure that will hold 
-the different locations.
-
-Arrays can store any data type. This time, your array will be storing objects.
-Objects are similar to arrays, but with a few differences.
-One difference is that objects use properties, or keys, to access and modify data.
-
-Objects are indicated by curly braces. An empty object would look like {}.
-
-Object properties are written as key: value pairs,
-where key is the name of the property (or the key),
-and value is the value that property holds.
-*/
-
 const locations = [
   {
     name: "town square",
@@ -115,42 +90,11 @@ const locations = [
   }
 ];
 
-/*
-The innerText property controls the text that appears in an HTML element.
-
-initialize buttons
-
-These elements have a special property called onclick, 
-which you can use to determine what happens when someone clicks that button.
-You can access properties in JavaScript a couple of different ways:
-The first is with dot notation.
-*/
-
+// initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-/* Functions are special tools that allow you to run sections of code 
-at specific times. 
-You can declare functions using the function keyword.
-
-When you have repetition in your code,
-this is a sign that you need another function.
-Functions can take parameters, which are values that
-are given to the function each time it is run.
-
-function goStore() {
-    console.log("Going to store.");
-}
-
-This is called bracket notation. 
-Values in an array are accessed by index.
-The innerHTML property allows you to access or modify the content inside 
-an HTML element using JavaScript:
-<p id="demo">This is a paragraph.</p>
-document.querySelector("#demo").innerHTML = "Hello, innerHTML!";
-*/  
-  
 function update(location) {
   monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
@@ -161,7 +105,7 @@ function update(location) {
   button3.onclick = location["button functions"][2];
   text.innerHTML = location.text;
 }
-  
+
 function goTown() {
   update(locations[0]);
 }
@@ -174,14 +118,6 @@ function goCave() {
   update(locations[2]);
 }
 
-/*
-Want to run code conditionally, you can use the if statement.
-An if statement is used to make decisions in code. The ketword if
-is followed by a condition in parentheses. If the condition is true, 
-the code inside the curly braces {} is executed. If the condition is false,
-the code inside the curly braces is skipped.
-*/
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -192,16 +128,6 @@ function buyHealth() {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
-
-/* You can insert variables into a string with the 
-concatenation operator +. 
-any time text.innerText was updated, the old text was erased. 
-This time, use the += operator to add text 
-Arrays have a length property that returns 
-the number of items in the array.
-You may want to add new values to the weapons array in the future.
-array indexing starts at zero.
-*/
 
 function buyWeapon() {
   if (currentWeapon < weapons.length - 1) {
@@ -222,18 +148,6 @@ function buyWeapon() {
     button2.onclick = sellWeapon;
   }
 }
-
-/*
-Notice that you already have a currentWeapon variable elsewhere in your code. 
-Since this new currentWeapon variable will be inside an if statement, 
-it will be scoped only to that block of code.
-Scope is the term used to describe where a variable can be accessed.
-If a variable is declared inside a block of code,
-it is only accessible to the code inside that block.
-This is called block scope.
-The shift() method on an array removes the first element in the array 
-and returns it.
-*/
 
 function sellWeapon() {
   if (inventory.length > 1) {
@@ -262,13 +176,6 @@ function fightDragon() {
   goFight();
 }
 
-/*
-By default, the HTML element that shows the monster's stats has been hidden
- with CSS. Use the style and display properties.
- style property is used to access the inline style of an element
- display property is used to set the visibility of an element
-*/
-
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
@@ -276,24 +183,6 @@ function goFight() {
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
-
-/*
-addition assignment operator(+=)
-The Math object in JavaScript contains static properties and methods for 
-mathematical constants and functions.
-One of those is Math.random(), which generates a random number from 
-0 (inclusive) to 1 (exclusive).
-Another is Math.floor(), which rounds a given number 
-down to the nearest integer.
-Using these, you can generate a random number within a range.
-health -= monsters[fighting].level; ->
-This sets health equal to health minus the return value of the 
-getMonsterAttackValue function, and passes the level of the monster 
-as an argument.
-  On every attack, there should be a chance that the player's weapon breaks.
-  inventory.pop(), which will remove the last item in the array 
-  AND return it so it appears in your string.
-*/
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
@@ -321,29 +210,11 @@ function attack() {
   }
 }
 
-/*
-Functions can return a value. This value can be assigned to a variable 
-and used elsewhere in your code.
-Ternary operator to ensure negative values are not returned.
-The ternary operator is a conditional operator and can be used as a 
-one-line if-else statement. 
-The syntax is: condition ? expressionIfTrue : expressionIfFalse.
-*/
-
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
-  // Log the value of hit to the console to use in debugging.
   console.log(hit);
-  // return hit;
   return hit > 0 ? hit : 0;
 }
-
-/*
-logical OR operator ||
-The logical OR operator will use the first value if it is truthy – that is, 
-anything apart from NaN, null, undefined, 0, -0, 0n, "", and false. 
-Otherwise, it will use the second value.
-*/
 
 function isMonsterHit() {
   return Math.random() > .2 || health < 20;
@@ -392,28 +263,6 @@ function pickTwo() {
 function pickEight() {
   pick(8);
 }
-
-/*
-A while loop accepts a condition, and will run the code in the block 
-until the condition is no longer true:
-while (i < 5) {
-
-}
-for loops are declared with three expressions separated by semicolons. 
-for (a; b; c), where a is the initialization expression, 
-b is the condition, and c is the final expression.
-The initialization expression is executed only once, 
-before the loop starts, and is often used to define 
-and set up the loop variable.
-Many for loops use i as the counter and start from 0
-The second statement in a for loop, the condition statement, 
-is evaluated at the beginning of every loop iteration.
-The loop will continue as long as the condition evaluates to be true.
-The last statement in a for loop, the final expression, 
-is executed at the end of each loop iteration.
-The .includes() method determines if an array contains an element and 
-will return either true or false.
-*/
 
 function pick(guess) {
   const numbers = [];
